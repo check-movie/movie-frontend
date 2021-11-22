ad9d975e897cd09516a244e45be5b8547ac3bb8c
not-raspy <54740570+not-raspy@users.noreply.github.com>
2021-11-22 21:01:31 +0100

    #4 refactoring

--- root/res/main.py
+++ root/res/main.py
@@ -1,49 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivymd.icon_definitions import md_icons
 from kivy.uix.image import Image
-from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 from kivymd.uix.menu import MDDropdownMenu
-<<<<<<< Updated upstream
-from kivy.metrics import dp
-
-
-class MainApp(MDApp):
-    dropdown = ObjectProperty()
-
-    def optionCallback(self, textOfTheOption):
-        print(textOfTheOption)
-
-    def build(self):
-        menu_items = [
-            {
-                "viewclass": "OneLineListItem",
-                "text": "Zaloguj się! ",
-                "height": dp(56),
-                "on_release": lambda x="Zaloguj się!": self.menu_callback(x),
-            },
-            {
-                "viewclass": "OneLineListItem",
-                "text": "Zarejestruj się!  ",
-                "height": dp(56),
-                "on_release": lambda x="Zarejestruj się! ": self.menu_callback(x),
-            },
-
-        ]
-        self.menu = MDDropdownMenu(
-            items=menu_items,
-            width_mult=4
-        )
-
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Red"
-        self.theme_cls.accent_palette = "Red"
-        return Builder.load_file('tbar.kv')
-=======
 
 
 class loginWindow(Screen):
@@ -52,29 +16,20 @@ class loginWindow(Screen):
 
 class signupWindow(Screen):
     pass
->>>>>>> Stashed changes
 
 
-
-class FirstWindow(Screen):
+class searchWindow(Screen):
     pass
 
 
-class SecondWindow(Screen):
+class windowManager(ScreenManager):
     pass
 
 
-<<<<<<< Updated upstream
-class MainScreen(GridLayout):
-    manager = ObjectProperty(None)
+kv = Builder.load_file('tbar.kv')
+sm = windowManager()
 
 
-class WindowManager(ScreenManager):
-    pass
-
-
-MainApp().run()
-=======
 # adding screens
 sm.add_widget(searchWindow(name='search'))
 sm.add_widget(loginWindow(name='login'))
@@ -88,4 +43,3 @@ class MainApp(MDApp):
 
 if __name__ == "__main__":
     MainApp().run()
->>>>>>> Stashed changes
